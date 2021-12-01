3b790907af9537bcf818ab00afdb3c0971afe776
Alonso271 <72246922+Alonso271@users.noreply.github.com>
2021-12-01 18:05:16 +0100

    Update funciones.php

--- EjerciciosAentregar/CRUD/app/funciones.php
+++ EjerciciosAentregar/CRUD/app/funciones.php
@@ -1,8 +1,9 @@
 <?php
 include_once 'app/config.php';
 
+// Cargo los datos segun el formato de configuración
 function cargarDatos(){
-    $funcion =__FUNCTION__.TIPO;
+    $funcion =__FUNCTION__.TIPO; // cargarDatostxt
     return $funcion();
 }
 
@@ -11,21 +12,28 @@ function volcarDatos($valores){
     $funcion($valores);
 }
 
+// ----------------------------------------------------
+// FICHERO DE TEXT 
+//Carga los datos de un fichero de texto
 function cargarDatostxt(){
+    // Si no existe lo creo
     $tabla=[]; 
     if (!is_readable(FILEUSER) ){
+        // El directorio donde se crea tiene que tener permisos adecuados
         $fich = @fopen(FILEUSER,"w") or die ("Error al crear el fichero.");
         fclose($fich);
     }
-    $fich = @fopen(FILEUSER, 'r') or die("ERROR al abrir fichero de usuarios");
+    $fich = @fopen(FILEUSER, 'r') or die("ERROR al abrir fichero de usuarios"); // abrimos el fichero para lectura
     
     while ($linea = fgets($fich)) {
         $partes = explode('|', trim($linea));
+        // Escribimos la correspondiente fila en tabla
         $tabla[]= [ $partes[0],$partes[1],$partes[2],$partes[3]];
         }
     fclose($fich);
     return $tabla;
 }
+//Vuelca los datos a un fichero de texto
 function volcarDatostxt($tvalores){
 
     $fich = @fopen(FILEUSER,"w") or die ("Error al escribir en el fichero.");
@@ -36,20 +44,28 @@ function volcarDatostxt($tvalores){
     fclose($fich);    
 }
 
+// ----------------------------------------------------
+// FICHERO DE CSV
+
 function cargarDatoscsv (){
+    // Si no existe lo creo
     $tabla=[];
     if (!is_readable(FILEUSER) ){
+        // El directorio donde se crea tiene que tener permisos adecuados
         $fich = @fopen(FILEUSER,"w") or die ("Error al crear el fichero.");
         fclose($fich);
     }
-    $fich = @fopen(FILEUSER, 'r') or die("ERROR al abrir fichero de usuarios");
+    $fich = @fopen(FILEUSER, 'r') or die("ERROR al abrir fichero de usuarios"); // abrimos el fichero para lectura
     
     while ($partes = fgetcsv($fich)) {
+        // Escribimos la correspondiente fila en tabla
         $tabla[]= [ $partes[0],$partes[1],$partes[2],$partes[3]];
     }
     fclose($fich);
     return $tabla;   
 }
+
+//Vuelca los datos a un fichero de csv
 function volcarDatoscsv($tvalores){
     
     $fich = @fopen(FILEUSER,"w") or die ("Error al escribir en el fichero.");
@@ -59,7 +75,10 @@ function volcarDatoscsv($tvalores){
     fclose($fich);
 }
 
+// ----------------------------------------------------
+// FICHERO DE JSON
 function cargarDatosjson (){
+    // Si no existe lo creo
     $tabla=[];
     $datosjson = @file_get_contents(FILEUSER) or die("ERROR al abrir fichero de usuarios");
     $tabla = json_decode($datosjson, true);   
@@ -75,10 +94,13 @@ function volcarDatosjson($tvalores){
 
 
 
+
+// MOSTRA LOS DATOS DE LA TABLA DE ALMACENADA EN AL SESSION 
 function mostrarDatos (){
     
     $titulos = [ "Nombre","login","Password","Comentario"];
     $msg = "<table>\n";
+     // Identificador de la tabla
     $msg .= "<tr>";
     for ($j=0; $j < CAMPOSVISIBLES; $j++){
         $msg .= "<th>$titulos[$j]</th>";
@@ -93,7 +115,7 @@ function mostrarDatos (){
         for ($j=0; $j < CAMPOSVISIBLES; $j++){
             $msg .= "<td>$datosusuario[$j]</td>";
         }
-        $msg .="<td><a href=\"#\" onclick=\"confirmarBorrar('$datosusuario[0]',$id);\" >Borrar</a></td>\n";
+        $msg .="<td><a href=\"".$auto."?orden=Borrar&id=$id\" onclick=\"confirmarBorrar('$datosusuario[0]',$id);\" >Borrar</a></td>\n";
         $msg .="<td><a href=\"".$auto."?orden=Modificar&id=$id\">Modificar</a></td>\n";
         $msg .="<td><a href=\"".$auto."?orden=Detalles&id=$id\" >Detalles</a></td>\n";
         $msg .="</tr>\n";
@@ -103,3 +125,23 @@ function mostrarDatos (){
    
     return $msg;    
 }
+
+/*
+ *  Funciones para limpiar la entreda de posibles inyecciones
+ */
+
+function limpiarEntrada(string $entrada):string{
+    $salida = trim($entrada); // Elimina espacios antes y después de los datos
+    $salida = stripslashes($salida); // Elimina backslashes \
+    $salida = htmlspecialchars($salida); // Traduce caracteres especiales en entidades HTML
+    return $salida;
+}
+// Función para limpiar todos elementos de un array
+function limpiarArrayEntrada(array &$entrada){
+ 
+    foreach ($entrada as $key => $value ) {
+        $entrada[$key] = limpiarEntrada($value);
+    }
+}
+
+
